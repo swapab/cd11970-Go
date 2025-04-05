70422bce005ba0155f4d3d4a5adbf83bc24865e9
Swapnil Abnave <swapnil.shantaram.abnave@cariad.technology>
2025-04-05 21:06:05 +0200

    static file server

--- L4. Building for the Web/4. Routing/Exercise/main.go
+++ L4. Building for the Web/4. Routing/Exercise/main.go
@@ -1,12 +1,20 @@
 package main
 
-// var members = map[string]string{
-// 	"1": "Andy",
-// 	"2": "Peter",
-// 	"3": "Gabriella",
-// 	"4": "Jordy",
-// }
+import "net/http"
+
+var members = map[string]string{
+	"1": "Andy",
+	"2": "Peter",
+	"3": "Gabriella",
+	"4": "Jordy",
+}
+
+func showContactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
 
 func main() {
-	// TODO
+	http.HandleFunc("/contact", showContactPage)
+	println("Starting Server...")
+	http.ListenAndServe(":3000", nil)
 }
